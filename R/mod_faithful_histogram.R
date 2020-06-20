338f56ed6daf6c92b71a9be5830378c0d92425fd
Monti_R_86 <monti1m@CORP.EDISON.IT>
2020-06-20 15:55:34 +0200

    stable refactoring

--- R/mod_faithful_histogram.R
+++ R/mod_faithful_histogram.R
@@ -39,17 +39,17 @@ mod_faithful_histogram_ui <- function(id) {
 #' @rdname mod_faithful_histogram
 #'
 #' @importFrom graphics hist
-mod_faithful_histogram_server <- function(input, output, session) {
+mod_faithful_histogram_server <- function(input, output, session,variable=rective(NULL)) {
   ns <- session$ns
 
   # generate bins based on input$bins from ui.R
-  x    <- datasets::faithful[, "waiting"]
-  bins <- reactive(seq(min(x), max(x), length.out = input$bins + 1))
+  x    <- reactive(datasets::faithful[, variable()])
+  bins <- reactive(seq(min(x()), max(x()), length.out = input$bins + 1))
 
   output$distPlot <- renderPlot({
     # draw the histogram with the specified number of bins
     hist(
-      x, breaks = bins(), freq = !input$density,
+      x(), breaks = bins(), freq = !input$density,
       main = "Histogram of waiting",
       col = "darkgray", border = "white"
     )
